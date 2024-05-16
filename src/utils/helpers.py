3bf940037cef249ebd5763870c248ab69ee43cde
Guilherme Sebastião <guivbsebas@gmail.com>
2024-05-16 17:13:39 -0300

    feat: add ordinal encoding function

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+
 
 def is_male(df, column):
     return df[column].apply(lambda x: 1 if x == 'male' else 0)
@@ -11,4 +13,13 @@ def apply_one_hot_encoding(df, columns) -> pd.DataFrame:
     ohe_df = pd.DataFrame(transformed_data, columns=ohe.get_feature_names_out())
     df = pd.concat([df, ohe_df], axis=1)
     return df
+
+
+def apply_ordinal_encoding(df):    
+    cat = ['S','C','Q']
+    ord_enc = OrdinalEncoder(categories=[cat],dtype='int32')
+    ord_enc = ord_enc.fit(df[['Embarked']])
+    df = ord_enc.transform(df[['Embarked']])
+
+    return df 
 
